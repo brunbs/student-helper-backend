c2860c0e5258c1d6f7b14c727abfcb679db7aa7c
Bruno Barbosa <babosa227@gmail.com>
2024-08-10 08:54:43 -0300

    initial commit

---
+++ src/main/java/br/com/bruno/barbosa/student_helper_backend/StudentHelperBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruno.barbosa.student_helper_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHelperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHelperBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bruno/barbosa/student_helper_backend/StudentHelperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bruno.barbosa.student_helper_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHelperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
